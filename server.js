a3aa33e432f25a89307e801c0c927cee60914ad8
etrusco74 <ademarchi74@hotmail.com>
2018-05-22 14:00:02 +0200

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Pensando app Hello World!');
+  res.send('Pensando app Hello World - first deploy!');
 });
 
 app.listen(3000, function () {
